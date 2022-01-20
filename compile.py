fb102e6686cec30ecf09a37f57b2ee06ec62a59c
ykao <ykao@cern.ch>
2022-01-20 12:55:53 +0800

    Update

--- compile.py
+++ compile.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import subprocess
 import sys
+import datetime
+today = datetime.datetime.today()
+datetime_tag = today.strftime("%Y%m%d")
 
 def move(f):
     subprocess.call("mv %s output/" % f, shell=True)
@@ -15,19 +18,23 @@ def redirect_ouput_files(f):
     move(stem + ".snm")
     move(stem + ".toc")
 
-def open_slides(f):
+def update_and_open_slides(f):
     stem = f.split('.')[0]
-    pdf_file = stem + ".pdf"
-    subprocess.call("open output/%s" % pdf_file, shell=True)
+    pdf_file_ori = stem + ".pdf"
+    pdf_file_mod = stem + "_" + datetime_tag + ".pdf"
+    subprocess.call("mv output/%s output/%s" % (pdf_file_ori, pdf_file_mod), shell=True)
+    subprocess.call("open output/%s" % pdf_file_mod, shell=True)
     
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print "Please specify a latex file."
+        mylatex = "tprime_hadronic_update.tex"
     else:
         mylatex = sys.argv[1]
-        subprocess.call("mkdir -p output", shell=True)
-        subprocess.call("pdflatex %s" % mylatex, shell=True)
-        redirect_ouput_files(mylatex)
-        open_slides(mylatex)
+
+    subprocess.call("mkdir -p output", shell=True)
+    subprocess.call("pdflatex %s" % mylatex, shell=True)
+    redirect_ouput_files(mylatex)
+    update_and_open_slides(mylatex)
+    print(">>> %s is used for making slides. :)" % mylatex)
